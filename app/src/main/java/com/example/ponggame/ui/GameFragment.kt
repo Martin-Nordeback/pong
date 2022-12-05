33d03323fb98c9acdc8917d09f39f67eb24c558c
Martin-Nordeback <martin.nordeback@gmail.com>
2022-12-05 16:32:17 +0100

    GameView update Martin

--- app/src/main/java/com/example/ponggame/ui/GameFragment.kt
+++ app/src/main/java/com/example/ponggame/ui/GameFragment.kt
@@ -1,25 +1,5 @@
 package com.example.ponggame.ui
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.example.ponggame.R
-import com.example.ponggame.utils.GameView
-
-
-class GameFragment : Fragment() {
-
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return GameView(activity as PongActivity)
-    }
-
+class GameFragment {
 
 }
--- app/src/main/java/com/example/ponggame/utils/GameView.kt
+++ app/src/main/java/com/example/ponggame/utils/GameView.kt
@@ -1,6 +1,129 @@
 package com.example.ponggame.utils
 
-class GameView {
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Rect
+import android.view.MotionEvent
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import com.example.ponggame.model.Ball
+import com.example.ponggame.ui.PongActivity
+import java.lang.System.currentTimeMillis
+import kotlin.concurrent.thread
+
+class GameView(context: Context) : SurfaceView(context), SurfaceHolder.Callback, Runnable {
+
+    private var score = 0
+    private var thread: Thread? = null
+    private var running = false
+    lateinit var canvas: Canvas
+
+    private lateinit var ball1: Ball
+
+    var pActivity = context as PongActivity
+    private var bounds = Rect()
+
+    var mHolder: SurfaceHolder? = holder
+
+    var frameRate = 30
+    var deltaTime = 0L
+    var timeToUpdate = currentTimeMillis()
+
+
+    init {
+        if (mHolder != null) {
+            mHolder?.addCallback(this)
+        }
+        setup()
+    }
+
+    fun setup() {
+        ball1 = Ball(this.context, 100f, 100f, 50f, 7f, 5f)
+        ball1.paint.color = Color.WHITE
+
+    }
+
+    fun start() {
+        running = true
+        thread = Thread(this)
+        thread?.start()
+    }
+
+    fun stop() {
+        running = false
+        try {
+            thread?.join()
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun update(speedAdjustment: Float) {
+        ball1.update(speedAdjustment)
+    }
+
+    fun draw() {
+        canvas = mHolder!!.lockCanvas()
+        canvas.drawColor(Color.BLACK)
+        ball1.draw(canvas)
+        mHolder!!.unlockCanvasAndPost(canvas)
+
+    }
+
+    fun intersects(b1: Ball, b2: Ball) {
+//        if (Math.sqrt(
+//                Math.pow(b1.posX - b2.posX.toDouble(), 2.0)
+//                        + Math.pow(b1.posY - b2.posY.toDouble(), 2.0)
+//            ) <= b1.size + b2.size
+//        ) {
+//            bounceBall(b1, b2)
+//            score ++
+//            pActivity.updateText("Score: $score")
+//        }
+    }
+
+    fun bounceBall(b1: Ball, b2: Ball) {
+        b1.speedY *= -1
+        b2.speedX = 0f
+        ball1.paint.color = Color.YELLOW
+
+    }
+
+
+    override fun surfaceCreated(p0: SurfaceHolder) {
+    }
+
+    override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun surfaceDestroyed(p0: SurfaceHolder) {
+        stop()
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return super.onTouchEvent(event)
+    }
+
+    override fun run() {
+        while (running) {
+            if (currentTimeMillis() >= timeToUpdate) {
+                deltaTime = currentTimeMillis() - timeToUpdate
+                timeToUpdate += 1000 / frameRate
+
+                val speedAdjustment = 1f + deltaTime * frameRate/1000
+
+
+                update(speedAdjustment)
+                draw()
+                intersects(ball1, ball2)
+                ball1.checkBounds(bounds)
+            }
+
+        }
+
+    }
+    }
 
 
 }
