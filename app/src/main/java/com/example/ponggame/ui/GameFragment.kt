c874f2211eb7df8795e43eb235185442b8246f91
filiphertzman <filip.hertzman@gmail.com>
2022-12-05 16:36:06 +0100

    Fragment 1

--- app/src/main/java/com/example/ponggame/ui/GameFragment.kt
+++ app/src/main/java/com/example/ponggame/ui/GameFragment.kt
@@ -1,5 +1,22 @@
 package com.example.ponggame.ui
 
-class GameFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.ponggame.utils.GameView
+
+class GameFragment: Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return GameView(activity as MainActivity)
+    }
+
 
 }
