f1f1b656f99c8689cbcdf452224754e784da19ca
bygxs <114972065+bygxs@users.noreply.github.com>
2022-12-06 09:39:27 +0100

    Player class created

--- app/src/main/java/com/example/ponggame/model/Player.kt
+++ app/src/main/java/com/example/ponggame/model/Player.kt
@@ -1,4 +1,47 @@
 package com.example.ponggame.model
 
-class Player {
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
+import android.view.WindowInsetsAnimation.Bounds
+
+class Player (context: Context, var posX: Float, var posY: Float, var size:Float, var speedX:Float, var speedY: Float,){
+    var paint = Paint()
+
+    fun update(speedAdjustment: Float){ // använda fingret eller nåt anat // update() is like setter
+        posY += speedY * speedAdjustment// posY = posY + speedY // posY += speed same same
+        posX += speedX * speedAdjustment
+
+
+    }
+
+    fun draw( canvas: Canvas?){ // this canvas draws the ball continuously. this can be null.
+        canvas?.drawCircle(posX,posY,size,paint) // drew() gets  brings the positions and send it to canvas. it is like getter
+    }
+    fun checkBounds(bounds: Rect){
+
+        if (posX-size < 0){   //  can be written bounds.left
+
+            //  this.posX += speedX*2 // this is to make sure it doesn't go away
+
+            this.speedX *= -1
+        }
+        if (posX+size > bounds.right){
+            this.speedX *= -1
+        }
+        if (posY-size < bounds.top){
+            this.speedY *= -1
+        }
+        if (posY+size > bounds.bottom){
+            this.speedY *= -1
+        }
+
+
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/ponggame/ui/GameFragment.kt
+++ app/src/main/java/com/example/ponggame/ui/GameFragment.kt
@@ -1,11 +1,10 @@
 package com.example.ponggame.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.ponggame.R
+import androidx.fragment.app.Fragment
 import com.example.ponggame.utils.GameView
 
 
