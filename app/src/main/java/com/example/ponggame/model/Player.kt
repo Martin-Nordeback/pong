5ca2e392ece4aab3380c222c2b128c38b8fd6440
filiphertzman <filip.hertzman@gmail.com>
2022-12-07 09:29:41 +0100

    player

--- app/src/main/java/com/example/ponggame/model/Player.kt
+++ app/src/main/java/com/example/ponggame/model/Player.kt
@@ -5,47 +5,66 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Rect
+import android.graphics.RectF
 import android.view.WindowInsetsAnimation.Bounds
 
-class Player (context: Context, var posX: Float, var posY: Float, var size:Float, var speedX:Float, var speedY: Float,){
-    var paint = Paint()
+class Player(private val screenSizeX: Int, private val screenSizeY: Int, onTop: Boolean) {
 
-    fun update(speedAdjustment: Float){ // använda fingret eller nåt anat // update() is like setter
-        posY += speedY * speedAdjustment// posY = posY + speedY // posY += speed same same
-        posX += speedX * speedAdjustment
+    val rect: RectF
 
+    private val playerBarLength: Float = (screenSizeX / 6).toFloat()
+    private val playerBarHeight: Float = (screenSizeY / 50).toFloat()
 
+    private var playerBarLeftX: Float = ((screenSizeX / 2) - (playerBarLength / 2))
+    private val playerBarTopY: Float = (screenSizeY - 20).toFloat()
+
+    private val playerBarSpeed: Float = screenSizeX.toFloat() * 2
+
+    val STOPPED = 0
+    val LEFT = 1
+    val RIGHT = 2
+
+    private var playerBarMoving = STOPPED
+
+    init {
+        rect = if (onTop) {
+            RectF(
+                playerBarLeftX, 35f,
+                playerBarLeftX + playerBarLength,
+                35f + playerBarHeight
+            )
+        } else {
+            RectF(
+                playerBarLeftX,
+                playerBarTopY - 55f,
+                playerBarLeftX + playerBarLength,
+                playerBarTopY + playerBarHeight - 55f
+            )
+        }
     }
 
-    fun draw( canvas: Canvas?){ // this canvas draws the ball continuously. this can be null.
-        canvas?.drawCircle(posX,posY,size,paint) // drew() gets  brings the positions and send it to canvas. it is like getter
+    fun playerBarMovementState(state: Int) {
+        playerBarMoving = state
     }
-    fun checkBounds(bounds: Rect){
 
-        if (posX-size < 0){   //  can be written bounds.left
-
-            //  this.posX += speedX*2 // this is to make sure it doesn't go away
-
-            this.speedX *= -1
+    fun playerBarUpdate(fps: Long) {
+        if (playerBarMoving == LEFT) {
+            playerBarLeftX = playerBarLeftX - playerBarSpeed / fps
         }
-        if (posX+size > bounds.right){
-            this.speedX *= -1
-        }
-        if (posY-size < bounds.top){
-            this.speedY *= -1
-        }
-        if (posY+size > bounds.bottom){
-            this.speedY *= -1
+        if (playerBarMoving == RIGHT) {
+            playerBarLeftX = playerBarLeftX + playerBarSpeed / fps
         }
 
+        if (rect.left < 0) {
+            playerBarLeftX = 0f
+        }
+        if (rect.right > screenSizeX) {
+            playerBarLeftX = screenSizeX - (rect.right - rect.left)
+        }
 
+        rect.left = playerBarLeftX
+        rect.right = playerBarLeftX + playerBarLength
 
     }
-// Player first commit failed due to token
-    // repeated fail to commit
-    //let's see
-
-  //comment for push request
-
-
 }
+
